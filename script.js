38b2f361fcd8cd3ac1294107d2904415c4fc3581
matteo banch <sebastien06400@hotmail.fr>
2020-11-04 15:52:54 -0800

    test

---
+++ script.js
@@ -0,0 +1,9 @@
+let stop = document.getElementById('stop')
+let cont = document.getElementById('cont')
+let butt = document.getElementById('butt')
+
+butt.addEventListener('click', () => { 
+    stop.style.display = 'block'
+    cont.style.display = 'none'
+
+});
